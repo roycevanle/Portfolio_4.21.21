2db4b9805fbb5dfdbc5486fd05910854e2decb93
roycevanle <roycele123@gmail.com>
2021-04-01 23:33:14 -0700

    updated package vulnerbilities & added personal notes as well as nav bar & collapasable button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavbarBrand from 'react-bootstrap/NavbarBrand';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Royce Le',
+            headerLinks: [
+              { title: 'Home', path: '/' },
+              { title: 'About', path: '/about' },
+              { title:  'Contact', path: '/contact' },
+            ],
+            home: {
+              title: 'Be Relentless',
+              subTitle: 'Project that make a difference',
+              text: 'Checkout my projects below'
+            },
+            about: {
+              title: 'About Me'
+            },
+            Contact: {
+              title: 'Let\'s Talk'
+            }
+        }
+    }
+  
+  render() {
+    return (
+        <Router>
+          {/* p-0 is for padding*/}
+          <Container className="p-0" fluid={true}> 
+            <Navbar className="border-bottom" bg="transparent" expand="lg">
+                <Navbar.Brand>Royce Le</Navbar.Brand>
+                {/* border-0 gets rid of the border of the hamburger menu */}
+                <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+                <Navbar.Collapse id="navbar-toggle">
+                  {/* ml-left is part of bootstrap that floats to right*/}
+                  <Nav className="ml-auto">
+                    {/* the 'to' prop acts as the url path*/}
+                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" to="/about">About</Link>
+                    <Link className="nav-link" to="/contact">Contact</Link>
+                  </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+
+
+
+             
+          </Container>
+        </Router>
+    );
+  }
+
 }
 
 export default App;
